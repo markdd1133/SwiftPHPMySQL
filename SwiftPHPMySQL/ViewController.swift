f58e1e318ed117ecc4fbbdd6583813cea7e00a4d
markdd1133 <shengchichen@Shengde-MacBook-Air.local>
2017-05-05 01:26:02 +0800

    it’s working

--- SwiftPHPMySQL/ViewController.swift
+++ SwiftPHPMySQL/ViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //URL to our web service
+    let URL_SAVE_TEAM = URL(string: "http://192.168.1.2/MyWebService/api/createteam.php")
+    
+    //Textfields declarations
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldMember: UITextField!
+    @IBOutlet weak var textFieldBrand: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Button action method
+    @IBAction func buttonSave(_ sender: Any) {
+        
+        //creating NSMutableURLRequest
+        let request = NSMutableURLRequest(url: URL_SAVE_TEAM!)
+        
+        //setting the method to post
+        request.httpMethod = "POST"
+        
+        //getting values from text fields
+        let teamName=textFieldName.text
+        let memberCount = textFieldMember.text
+        let teamBrand = textFieldBrand.text
+        
+        //creating the post parameter by concatenating the keys and values from text field
+        let postParameters = "name="+teamName!+"&member="+memberCount!+"&brand="+teamBrand!;
+        
+        //adding the parameters to request body
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        
+        //creating a task to send the post request
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if let err = error as NSError?{
+                print("error is \(err)")
+                return;
+            }
+            
+            //parsing the response
+            do {
+                //converting resonse to NSDictionary
+                let myJSON = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                
+                //parsing the json
+                if let parseJSON = myJSON {
+                    
+                    //creating a string
+                    var msg : String!
+                    
+                    //getting the json response
+                    msg = parseJSON["message"] as! String?
+                    
+                    //printing the response
+                    print(msg)
+                    
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        //executing the task
+        task.resume()
     }
-
-
+    
 }
 
